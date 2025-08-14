fe4422adc25abf76e02bf2dc1a9de126956e04e8
glsunnykumar <glsunny.kumar@gmail.com>
2025-08-14 18:29:58 +0530

    add trash icon

--- scripts/cart.js
+++ scripts/cart.js
@@ -25,7 +25,11 @@ function displayCart() {
             <span class="item-price">₹${item.price}</span>
             <input type="number" min="1" value="${item.quantity}" onchange="updateQuantity(${index}, this.value)">
             <span class="item-total">₹${item.price * item.quantity}</span>
-            <button class="remove" onclick="removeItem(${index})">❌</button>
+            <button class="remove" onclick="removeItem(${index})" title="Remove item">
+                <svg xmlns="http://www.w3.org/2000/svg" height="20" width="20" viewBox="0 0 448 512" fill="currentColor">
+                    <path d="M135.2 17.7L141.7 0h164.6l6.5 17.7L328 32H448v32H0V32h120L135.2 17.7zM32 96h384l-28.7 384.5c-1.6 22.1-20 39.5-42.2 39.5H103c-22.2 0-40.6-17.4-42.2-39.5L32 96z"/>
+                </svg>
+            </button>
         `;
         cartContainer.appendChild(itemElement);
     });
